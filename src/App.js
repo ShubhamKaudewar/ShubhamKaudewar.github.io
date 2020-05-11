46e70336117ead9c23aa36c177fb621ce6d0cf40
ShubhamKaudewar <kaudewar@gmail.com>
2020-05-11 17:40:46 +0530

    Push portfolio code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Sidebar from './Components/Sidebar';
+import Landing from './Components/Landing';
+import Experience from './Components/Experience';
+import Education from './Components/Education';
+import Skills from './Components/Skills';
+import Interests from './Components/Interests';
+import Awards from './Components/Awards';
+import profileData from './profileData.json';
+import ReactGA from 'react-ga';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      landingData : profileData.landing,
+      experience : profileData.experience,
+      education : profileData.education,
+      skills : profileData.skills,
+      interests : profileData.interests,
+      awards : profileData.awards
+    };
+
+    ReactGA.initialize('UA-131088172-2');
+    ReactGA.pageview(window.location.pathname);
+  }
+  render() {
+    return (
+      <div className="App">
+        <Sidebar sidebarData={this.state.landingData} />
+        <div className="container-fluid p-0">
+          <Landing landingData={this.state.landingData} />
+          <hr className="m-0" />
+          <Experience experience={this.state.experience} />
+          <hr className="m-0" />
+          <Education education={this.state.education}/>
+          <hr className="m-0" />
+          <Skills skills={this.state.skills} />
+          <hr className="m-0" />
+          <Interests interests={this.state.interests} />
+          <hr className="m-0" />
+          <Awards awards={this.state.awards} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
