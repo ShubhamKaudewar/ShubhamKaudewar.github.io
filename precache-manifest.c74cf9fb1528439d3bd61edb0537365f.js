075a399d88261d7dbdbe4d7491087c6c88834d51
ShubhamKaudewar <kaudewar@gmail.com>
2020-05-11 17:39:59 +0530

    Updates

---
+++ precache-manifest.c74cf9fb1528439d3bd61edb0537365f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8116aa8a21f7a8f0c5fa87771da1d253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "884e6a338e780a58cbaf",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c15ed118608e0d09edc1",
+    "url": "/static/js/2.4b2e0b2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b2e0b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "884e6a338e780a58cbaf",
+    "url": "/static/js/main.282cbdab.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "f8a9c6e7c6e92e1e5351b5255482c8d7",
+    "url": "/static/media/profile.f8a9c6e7.jpeg"
+  }
+]);
