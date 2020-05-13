312a0aac3d9588bdea87c785dda5fa75aa6aac6e
ShubhamKaudewar <kaudewar@gmail.com>
2020-05-13 17:02:41 +0530

    NewBuild3

---
+++ build/precache-manifest.51ccf45575f0b6097f431a6efd35dc36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d52077824eb60439c238fcff02b5abe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20a44a8b51986c655149",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a7a009c4c1098656f02d",
+    "url": "/static/js/2.354cdfb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.354cdfb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a44a8b51986c655149",
+    "url": "/static/js/main.bf838aee.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "f8a9c6e7c6e92e1e5351b5255482c8d7",
+    "url": "/static/media/profile.f8a9c6e7.jpeg"
+  }
+]);
