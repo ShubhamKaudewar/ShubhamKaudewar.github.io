c7cb5195b10d17ff9eca169e679b51bfb8f46946
Shubham Kaudewar <you@example.com>
2024-11-18 19:04:02 +0530

    new build

---
+++ build/precache-manifest.5d2f9c4e1b230e186187bf61ffdd7720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4784f81d3d5d76b078d018cc8f07e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50d135b68ebd1ce31a8f",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "8196d7b223ba1e19e234",
+    "url": "/static/js/2.1f8e09b1.chunk.js"
+  },
+  {
+    "revision": "50d135b68ebd1ce31a8f",
+    "url": "/static/js/main.e32bcbd4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f8a9c6e7c6e92e1e5351b5255482c8d7",
+    "url": "/static/media/profile.f8a9c6e7.jpeg"
+  }
+]);
