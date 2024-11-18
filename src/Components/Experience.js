bcbd2def00bce274bbc8a191f572a5e158a91332
Shubham Kaudewar <you@example.com>
2024-11-18 19:00:46 +0530

    new changes

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -18,7 +18,7 @@ class Experience extends Component {
                 <div className="resume-content">
                   <h3 className="mb-0">{exp.position}</h3>
                   <div className="subheading mb-3">{exp.organization}</div>
-                  <p>{exp.aboutWork}</p>
+                  <ul> {exp.aboutWork.map((point, i) => ( <li key={i}>{point}</li> ))} </ul>
                 </div>
                 <div className="resume-date text-md-right">
                   <span className="text-primary">{exp.fromDate} - {exp.toDate}</span>
